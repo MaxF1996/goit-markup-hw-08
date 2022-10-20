ce0f73a076d208a53970c9b767d6212b26502abd
MaxF1996 <maxeffect.apollo@gmail.com>
2022-10-20 15:56:31 +0300

    delete attribute for body (validator cause)

--- js/modal.js
+++ js/modal.js
@@ -1,16 +1,17 @@
 (() => {
   const refs = {
+    page: document.querySelector('.scroll-on'),
     openModalBtn: document.querySelector('[data-modal-open]'),
     closeModalBtn: document.querySelector('[data-modal-close]'),
     modal: document.querySelector('[data-modal]'),
-    page: document.querySelector('[page]'),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
-    refs.modal.classList.toggle('backdrop--is-hidden');
+    refs.page.classList.toggle('scroll-on');
     refs.page.classList.toggle('no-scroll');
+    refs.modal.classList.toggle('backdrop--is-hidden');
   }
 })();
